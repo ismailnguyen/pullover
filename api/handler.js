af8221a6055e3c76020a695301f8e3fc74967eeb
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2023-01-10 11:59:45 +0100

    change response message formats

--- api/handler.js
+++ api/handler.js
@@ -5,15 +5,13 @@ import { getReport } from './response_time_report.js';
 export default function handler(request, response) {
     if (request.query.key !== process.env.ACCESS_KEY) {
         response.status(401).json({
-            body: 'Invalid key'
+            error: 'Invalid key'
         });
 
         return;
     }
 
     getReport(lastWeekDate(), (data) => {
-        response.status(200).json({
-            body: data
-        });
+        response.status(200).json(data);
     });
 }
