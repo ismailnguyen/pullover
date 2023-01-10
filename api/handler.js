00da813b4b21fec428b9f9decb62237efbf56ce9
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2023-01-10 11:50:46 +0100

    protect endpoint with access key

--- api/handler.js
+++ api/handler.js
@@ -1,8 +1,15 @@
 
-import { lastWeek } from "date_helper.js";
-import { getReport } from "response_time_report.js";
+import { lastWeek } from './date_helper.js';
+import { getReport } from './response_time_report.js';
 
 export default function handler(request, response) {
+    if (process.env.ACCESS_KEY) {
+        response.status(401).json({
+            body: 'Invalid key'
+        });
+
+        return;
+    }
     getReport(lastWeek(), (data) => {
         response.status(200).json({
             body: data
