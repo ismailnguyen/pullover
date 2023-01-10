539be7fa0e24f757cac360a3c50e64155ea77f31
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2023-01-10 11:56:15 +0100

    fix access key comparison

--- api/handler.js
+++ api/handler.js
@@ -3,13 +3,14 @@ import { lastWeekDate } from './date_helper.js';
 import { getReport } from './response_time_report.js';
 
 export default function handler(request, response) {
-    if (request.query.key === process.env.ACCESS_KEY) {
+    if (request.query.key !== process.env.ACCESS_KEY) {
         response.status(401).json({
             body: 'Invalid key'
         });
 
         return;
     }
+
     getReport(lastWeekDate(), (data) => {
         response.status(200).json({
             body: data
